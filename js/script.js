12dac7630a58bcc44159ac89b08a3459fd12d8cf
hideki watabe <hideki.w.1971@gmail.com>
2022-03-04 01:01:00 +0900

    2022/3/4 やっと動くようになった。レスポンシブは未対応（パラメータで breakpoint を設定するだけ見たい。）

--- js/script.js
+++ js/script.js
@@ -2,43 +2,14 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
 
-  $(function () {
-    // スクロールの方向　-1の時は左、1の時は右
-    var dir = -1;
-
-    // スクロールのインターバル(何秒ごとにスクロールさせるか。3000ミリ秒に設定)
-    var interval = 3000;
-
-    // スクロールのスピード(700ミリ秒に設定）
-    var duration = 700;
-
-    // タイマー用の変数
-    var timer;
-
-    // リストの順番を変更(3番目を1番最初にする)
-    $("#slide ul").prepend($("#slide li:last-child"));
-
-    // リストの位置を変更(画像1枚分ずらす)
-    $("#slide ul").css("left", -375);
-
-    // 3000ミリ秒(変数intervalの値)ごとにslideTime()関数を実行
-    timer = setInterval(slideTimer, interval);
-
-    // slideTimer()関数
-    function slideTimer() {
-      // 画像1枚分左へスクロール
-      $("#slide ul").animate({ "left" : "-=375px" }, duration,
-      function() {
-        // リストの順番を変更
-        $(this).append($("#slide li:first-child"));
-
-        // リストの位置を変更
-        $(this).css("left", -375);
+    $(document).ready(function(){
+      $('.slider').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true
+        // responsiveのbreakpoint設定もある。ここで設定する。
       });
-
-    }
-
-  });
+    });
 
 
 });
